5218908995ab3626ab30562ae21ec8cafbfdc8e0
01soni247 <01soni247@gmail.com>
2022-06-10 15:02:15 +0000

    lava:Kscope-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/lineage_lava.mk
+   $(LOCAL_DIR)/kscope_lava.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lava-user \
-    lineage_lava-userdebug \
-    lineage_lava-eng
+    kscope_lava-user \
+    kscope_lava-userdebug \
+    kscope_lava-eng
